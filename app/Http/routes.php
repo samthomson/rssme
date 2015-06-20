2132de123a7f5884f97141a5bafdc7893717f19b
unknown <samt@samt.st>
2015-06-20 17:27:08 +0200

    couple of views in with routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,5 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	// make app view or force them to login/register
+    return view('app/home');
+});
+
+Route::get('/login', function () {
+	//  login/register
+    return view('app/login');
 });
