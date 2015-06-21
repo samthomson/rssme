1d339d573a231be50553726111ae36577b1b1994
unknown <samt@samt.st>
2015-06-21 17:57:55 +0200

    delete feeds

--- app/Http/Controllers/Feeds.php
+++ app/Http/Controllers/Feeds.php
@@ -11,9 +11,10 @@ use App\Http\Controllers\Controller;
 
 use Illuminate\Support\Facades\Request;
 
+use Auth;
+
 class Feeds extends Controller
-{
-    
+{    
 
     public static function create()
     {
@@ -22,15 +23,19 @@ class Feeds extends Controller
             $oFeed = new Feed;
 
             $oFeed->url = Request::get('feedurl');
+            $oFeed->user_id = Auth::id();
 
             $oFeed->save();
             return redirect('/');
         }
     }
 
-
     public static function delete($id)
     {
-        
+        $oFeed = Auth::user()->feeds()->where("id", $id);
+        if($oFeed){
+            $oFeed->delete();
+        }
+        return redirect('/feeds/manage');
     }
 }
--- resources/views/app/feeds/manage.blade.php
+++ resources/views/app/feeds/manage.blade.php
@@ -10,7 +10,12 @@
 
 	@if(count($oFeeds))
 		@foreach ($oFeeds as $oFeed)
-			{{$oFeed->url}} <form>delete</form><br/>
+			{{$oFeed->url}}
+			<form action="/feeds/{{$oFeed->id}}" method="post">
+				<input type="hidden" name="_method" value="DELETE">
+				<input type="hidden" name="_token" value="{{ csrf_token() }}">
+				<input type="submit" value="delete">
+			</form><br/>
 		@endforeach
 	@else
 		no feeds
