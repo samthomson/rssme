62fd18f9af5829ef018a229731530c62b2c4f614
unknown <samt@samt.st>
2015-06-24 18:49:56 +0200

    ui tweeks

--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -12,44 +12,46 @@
 
 
 			@if(count($oaFeedItems))
-				<table class="table table-condensed">
-					<!--
-					<thead>
-				        <tr>
-							<td>thumb</td>
-							<td>name</td>
-							<td>title</td>
-							<td>date</td>
-				        </tr>
-				    </thead>
-				    -->
-		    		<tbody>
-					
-						@foreach($oaFeedItems as $oItem)
-							<?php
-								$sPic = $oItem->thumb !== '' ? $oItem->thumb : $oItem->feedthumb;
+				<div class="table-responsive">
+					<table class="table table-condensed">
+						<!--
+						<thead>
+					        <tr>
+								<td>thumb</td>
+								<td>name</td>
+								<td>title</td>
+								<td>date</td>
+					        </tr>
+					    </thead>
+					    -->
+			    		<tbody>
+						
+							@foreach($oaFeedItems as $oItem)
+								<?php
+									$sPic = $oItem->thumb !== '' ? $oItem->thumb : $oItem->feedthumb;
 
-								$oLastHit = new Carbon\Carbon($oItem->date);
-								$sSince = $oLastHit->diffForHumans();
+									$oLastHit = new Carbon\Carbon($oItem->date);
+									$sSince = $oLastHit->diffForHumans();
 
-								$sStartLink = '<a target="_blank" class="feed-item" href="'.$oItem->url.'"><span>';
-								$sEndLink = '</span></a>';
-							?>
-							<tr class="feed-item">
+									$sStartLink = '<a target="_blank" class="feed-item" href="'.$oItem->url.'"><span>';
+									$sEndLink = '</span></a>';
+								?>
+								<tr class="feed-item">
 
 
-								<td><?php echo $sStartLink; ?><span class="circle" style="background-color:{{$oItem->feed_colour or ''}};"></span></td>
-								<td><?php echo $sStartLink; ?><img src="{{$sPic}}" class="feed-thumb"/><?php echo $sEndLink; ?></td>
-								<td><?php echo $sStartLink; ?>{{$oItem->name}}<?php echo $sEndLink; ?></td>
-								<td><?php echo $sStartLink; ?>
-							{{$oItem->title}}<?php echo $sEndLink; ?></td>
-							
-								<td title="{{$oLastHit->toDayDateTimeString()}}"><?php echo $sStartLink; ?>{{$sSince}}<?php echo $sEndLink; ?></td>
-							
-							</tr>
-						@endforeach
-					</tbody>
-				</table>
+									<td><?php echo $sStartLink; ?><span class="circle" style="background-color:{{$oItem->feed_colour or ''}};"></span></td>
+									<td><?php echo $sStartLink; ?><img src="{{$sPic}}" class="feed-thumb"/><?php echo $sEndLink; ?></td>
+									<td><?php echo $sStartLink; ?>{{$oItem->name}}<?php echo $sEndLink; ?></td>
+									<td><?php echo $sStartLink; ?>
+								{{$oItem->title}}<?php echo $sEndLink; ?></td>
+								
+									<td title="{{$oLastHit->toDayDateTimeString()}}"><?php echo $sStartLink; ?>{{$sSince}}<?php echo $sEndLink; ?></td>
+								
+								</tr>
+							@endforeach
+						</tbody>
+					</table>
+				</div>
 
 				<?php echo $oaFeedItems->render(); ?>
 
