6af382124b8641d24973f02cbe8e6d3e02ef77fb
unknown <samt@samt.st>
2015-07-29 15:35:59 +0200

    mobile tweeks

--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -16,11 +16,11 @@
 
 				<a ng-repeat="feeditem in feeditems" target="_blank" class="feed-item" href="@{{feeditem.url}}">
 					<div class="row feed-item">
-						<div class="col-xs-1">
+						<div class="col-xs-2 col-sm-1">
 							<img class="feed-thumb" ng-src="@{{feeditem.thumb}}" />
 						</div>
 
-						<div class="col-xs-1 col-sm-1">@{{feeditem.name}}</div>
+						<div class="col-xs-0 col-sm-1 hidden-xs">@{{feeditem.name}}</div>
 
 						<div class="col-xs-10 col-sm-9">@{{feeditem.title}}</div>
 
@@ -28,9 +28,13 @@
 					</div>
 				</a>
 
-				<div>
-					<a class="pagination btn btn-default" ng-show="iPage > 1" ng-click="iPage = iPage -1"><i class="fa fa-caret-left"></i> newer</a>
-					<a class="pagination btn btn-default" ng-click="iPage = iPage  +1">older <i class="fa fa-caret-right"></i></a>
+				<div class="row">
+					<div class="col-xs-6">
+						<a class="pagination btn btn-default form-control" ng-show="iPage > 1" ng-click="iPage = iPage -1"><i class="fa fa-caret-left"></i> newer</a>
+					</div>
+					<div class="col-xs-6">
+						<a class="pagination btn btn-default form-control" ng-click="iPage = iPage  +1">older <i class="fa fa-caret-right"></i></a>
+					</div>
 				</div>
 
 			</div>
