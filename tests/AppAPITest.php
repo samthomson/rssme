2eb129d0430ebdae8d478bd5e417d1c5c135ea4c
unknown <samt@samt.st>
2015-08-15 20:14:12 +0200

    added a test

---
+++ tests/AppAPITest.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class AppAPITest extends TestCase
+{
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testResponseIsJson()
+    {
+        /*
+        $response = $this->call('GET', '/');
+
+        $this->assertEquals(200, $response->status());
+        */
+        
+
+        $this->visit('/app/user/feedsandcategories')
+             ->seeJson();
+        /*     */
+    }
+}
