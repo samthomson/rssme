c14d07ebfb6e053182af7c9a0546ed2f85998e06
unknown <samt@samt.st>
2015-08-20 22:34:15 +0200

    added minification packages, does nothing so far..

--- config/app.php
+++ config/app.php
@@ -146,6 +146,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Intervention\Image\ImageServiceProvider::class,
+        GrahamCampbell\HTMLMin\HTMLMinServiceProvider::class,
 
     ],
 
@@ -196,6 +197,9 @@ return [
         'View'      => Illuminate\Support\Facades\View::class,
 
         'Image'     => Intervention\Image\Facades\Image::class,
+
+        'HTMLMin' => GrahamCampbell\HTMLMin\Facades\HTMLMin::class,
+
 
     ],
 
